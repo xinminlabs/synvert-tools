aa75ef7129e39850f873cb9cecaf8391863ced2f
Richard Huang <flyerhzm@gmail.com>
2014-09-15 20:36:22 +0800

    use eval for parsing rules

--- lib/synvert_tools.rb
+++ lib/synvert_tools.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 class SynvertTools
   def self.matching_code(code, str_rules)
     node = self.to_ast_node code
-    rules = self.to_hash_rules str_rules
+    rules = eval("{#{str_rules}}")
 
     matching_nodes = []
 
@@ -48,12 +48,5 @@ class SynvertTools
       file.close
       file.unlink
     end
-  end
-
-  # type: 'send', message: 'create
-  # =>
-  # {"type": "send", "message": "create"}
-  def self.to_hash_rules(str_rules)
-    ActiveSupport::JSON.decode('{"' + str_rules.gsub(':', '":').gsub(/, ?/, ', "').gsub("'", '"') + '}')
   end
 end
