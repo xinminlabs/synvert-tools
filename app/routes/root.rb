8107380201bcdb129de5da04db54789a2953d5dd
Richard Huang <flyerhzm@gmail.com>
2014-09-18 01:00:17 +0900

    rescue Parser::SyntaxError

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -13,44 +13,59 @@ module SynvertToolsApp
         code = params[:code].presence
         snippet = params[:snippet].presence
 
-        result = SynvertTools.convert_code(code, snippet)
+        begin
+          result = SynvertTools.convert_code(code, snippet)
 
-        if result
-          result = result.gsub(/\n/, '<br>')
-          result = result.gsub(/( )/, '&nbsp;')
+          if result
+            result = result.gsub(/\n/, '<br>')
+            result = result.gsub(/( )/, '&nbsp;')
+          end
+
+          content_type :json
+          { result: result }.to_json
+        rescue Parser::SyntaxError
+          status 500
+          {}.to_json
         end
-
-        content_type :json
-        { result: result }.to_json
       end
 
       post '/convert' do
         code = params[:code].presence
 
-        result = SynvertTools.to_ast_node(code).to_sexp
+        begin
+          result = SynvertTools.to_ast_node(code).to_sexp
 
-        if result
-          result = result.gsub(/\n/, '<br>')
-          result = result.gsub(/( )/, '&nbsp;')
+          if result
+            result = result.gsub(/\n/, '<br>')
+            result = result.gsub(/( )/, '&nbsp;')
+          end
+
+          content_type :json
+          { result: result }.to_json
+        rescue Parser::SyntaxError
+          status 500
+          {}.to_json
         end
-
-        content_type :json
-        { result: result }.to_json
       end
 
       post '/match' do
         code = params[:code].presence
         rule = params[:rule].presence
 
-        matchings = SynvertTools.matching_code(code, rule) if code && rule
+        begin
+          matchings = SynvertTools.matching_code(code, rule) if code && rule
 
-        if matchings
-          matchings[1] = matchings[1].gsub(/\n/, '<br>')
-          matchings[1] = matchings[1].gsub(/(  )/, '&nbsp;&nbsp;')
+          if matchings
+            matchings[1] = matchings[1].gsub(/\n/, '<br>')
+            matchings[1] = matchings[1].gsub(/(  )/, '&nbsp;&nbsp;')
+          end
+
+          content_type :json
+          { matchings: matchings || [] }.to_json
+        rescue Parser::SyntaxError
+          status 500
+          {}.to_json
         end
-
-        content_type :json
-        { matchings: matchings || [] }.to_json
       end
     end
   end
